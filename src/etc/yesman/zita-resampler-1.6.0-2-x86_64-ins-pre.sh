bd43b10898058950c2081304d183db28a5aecb24
Alexandru Thirtheu <alex_giusi_tiri2@yahoo.com>
2018-03-08 19:55:48 -0500

    Initial version

---
+++ src/etc/yesman/zita-resampler-1.6.0-2-x86_64-ins-pre.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# If required,
+# run commands from the in-between versions.
+#yesman-pre-inter "${repo}" "${pkg_name}" "${ver_dest}" "${ver_src}" "${arch}" '*';
+# Or,
+# a specific version.
+#"${yesman_cfg_hookdir}/""${repo}-${pkg_name}-1.5.9-1-${arch}";
+
+# Run the precommands specific for this version.
+#rm -rf /usr/lib/libzita-resampler.so.1;
+echo "${0}";
+exit 2;
---
+++ src/usr/bin/yesman-ins-post.sh
@@ -0,0 +1,107 @@
+#! /bin/bash
+
+
+#local repo pkg_name ver_dest ver_src arch;
+# To test the input format.
+#i=0;
+while read pkg_name;
+do
+	#echo "${i}::${pkg_name}";
+	#i=$((++i));
+	
+	
+	# Retrieve some values.
+	#repo='';
+	arch=$(pacman --query --info "${pkg_name}" | grep 'Architecture' - | cut --delimiter=':' --fields=2 | xargs);
+	# Because this script would only be triggered when reinstalling, downgrading, or upgrading,
+	# a version is expected to always be installed.
+	ver_src=$(pacman --query --info "${pkg_name}" | grep 'Version' - | cut --delimiter=':' --fields=2 | xargs);
+	ver_dest=$(pacman --sync --info "${pkg_name}" | grep 'Version' - | cut --delimiter=':' --fields=2 | xargs);
+	
+	# Do not activate when reinstalling the same version.
+	if test "${ver_dest}" == "${ver_src}";
+	then
+		continue;
+	fi;
+	
+	# Run the first found executable file.
+	
+	# This package; this version; this architecture.
+	if test -x "/etc/yesman/${pkg_name}-${ver_dest}-${arch}-ins-post.sh";
+	then
+		# Run it.
+		echo -e -n "Running '""/etc/yesman/${pkg_name}-${ver_dest}-${arch}-ins-post.sh""'...\n";
+		"/etc/yesman/${pkg_name}-${ver_dest}-${arch}-ins-post.sh";
+		
+		error=${?};
+		if test ${error} -ne 0;
+		then
+			echo "exiting[${error}]";
+			exit ${error};
+		fi;
+		echo "exit[${error}]";
+	
+	# This package; any version; this architecture.
+	elif test -x "/etc/yesman/${pkg_name}-any-${arch}-ins-post.sh";
+	then
+		# Run it.
+		echo -e -n "Running '""/etc/yesman/${pkg_name}-any-${arch}-ins-post.sh""'...\n";
+		"/etc/yesman/${pkg_name}-any-${arch}-ins-post.sh";
+		
+		error=${?};
+		if test ${error} -ne 0;
+		then
+			echo "exiting[${error}]";
+			exit ${error};
+		fi;
+		echo "exit[${error}]";
+	
+	# This package; this version; any architecture.
+	elif test -x "/etc/yesman/${pkg_name}-${ver_dest}-any-ins-post.sh";
+	then
+		# Run it.
+		echo -e -n "Running '""/etc/yesman/${pkg_name}-${ver_dest}-any-ins-post.sh""'...\n";
+		"/etc/yesman/${pkg_name}-${ver_dest}-any-ins-post.sh";
+		
+		error=${?};
+		if test ${error} -ne 0;
+		then
+			echo "exiting[${error}]";
+			exit ${error};
+		fi;
+		echo "exit[${error}]";
+	
+	# This package; any version; any architecture.
+	elif test -x "/etc/yesman/${pkg_name}-any-any-ins-post.sh";
+	then
+		# Run it.
+		echo -e -n "Running '""/etc/yesman/${pkg_name}-any-any-ins-post.sh""'...\n";
+		"/etc/yesman/${pkg_name}-any-any-ins-post.sh";
+		
+		error=${?};
+		if test ${error} -ne 0;
+		then
+			echo "exiting[${error}]";
+			exit ${error};
+		fi;
+		echo "exit[${error}]";
+	
+	# Any package; any version; any architecture.
+	elif test -x "/etc/yesman/any-any-any-ins-post.sh";
+	then
+		# Run it.
+		echo -e -n "Running '""/etc/yesman/any-any-any-ins-post.sh""'...\n";
+		"/etc/yesman/any-any-any-ins-post.sh";
+		
+		error=${?};
+		if test ${error} -ne 0;
+		then
+			echo "exiting[${error}]";
+			exit ${error};
+		fi;
+		echo "exit[${error}]";
+	fi;
+done < /dev/stdin;
+
+
+#exit 1;
