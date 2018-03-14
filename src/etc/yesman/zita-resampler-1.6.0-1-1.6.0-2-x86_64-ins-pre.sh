ef4b3a7c486778adc34608b7994d37ba038e46e4
Alexandru Thirtheu <alex_giusi_tiri2@yahoo.com>
2018-03-13 21:42:48 -0400

    Reduced code duplication. Converted from pacman hooks into external command(s). Made individual tools for each step of the update process. Copied from the script `checkupdates`, using it for inspiration.

--- src/etc/yesman/zita-resampler-1.6.0-1-1.6.0-2-x86_64-ins-pre.sh
+++ src/etc/yesman/zita-resampler-1.6.0-1-1.6.0-2-x86_64-ins-pre.sh
@@ -3,8 +3,8 @@
 # An example.
 echo "${0} ${@}";
 
-echo -e -n '`rm -rf /usr/lib/libzita-resampler.so.1`...'"\n";
+echo -e -n '`rm /usr/lib/libzita-resampler.so.1`...'"\n";
 # Run the commands specific for this repo/pkg-ver->ver-arch.
-rm -rf /usr/lib/libzita-resampler.so.1;
+rm /usr/lib/libzita-resampler.so.1;
 
-#exit 2;
+exit 0;
